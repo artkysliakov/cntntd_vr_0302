0436f1587b9972a473577d45939702ab4807ba29
artem88 <artemik2010@gmail.com>
2018-06-16 19:59:47 +0300

    Создан и настроен проект Unity

---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    [Header("Bouncy Target")]
+    public int maxBouncyTargetsCount = 2;
+    private int currentBouncyTargetsCount = 0;
+    public Transform bouncyRespawnPoint;
+    public GameObject bouncyTargetPrefab;
+
+    [Header("Heavy Target")]
+    public int maxHeavyTargetsCount = 2;
+    private int currentHeavyTargetsCount = 0;
+    public Transform heavyRespawnPoint;
+    public GameObject heavyTargetPrefab;
+
+    [Header("Cilinder Target")]
+    public int maxCilinderTargetsCount = 2;
+    private int currentCilinderTargetsCount = 0;
+    public Transform cilinderRespawnPoint;
+    public GameObject cilinderTargetPrefab;
+
+    public void InstantiateBouncyTarget()
+    {
+        if(currentBouncyTargetsCount < maxBouncyTargetsCount)
+        {
+            Instantiate(bouncyTargetPrefab, bouncyRespawnPoint.position, bouncyRespawnPoint.rotation);
+            currentBouncyTargetsCount++;
+        }
+    }
+
+    public void InstantiateHeavyTarget()
+    {
+        if (currentHeavyTargetsCount < maxHeavyTargetsCount)
+        {
+            Instantiate(heavyTargetPrefab, heavyRespawnPoint.position, heavyRespawnPoint.rotation);
+            currentHeavyTargetsCount++;
+        }
+    }
+
+    public void InstantiateCilinderTarget()
+    {
+        if (currentCilinderTargetsCount < maxCilinderTargetsCount)
+        {
+            Instantiate(cilinderTargetPrefab, cilinderRespawnPoint.position, cilinderRespawnPoint.rotation);
+            currentCilinderTargetsCount++;
+        }
+    }
+}
